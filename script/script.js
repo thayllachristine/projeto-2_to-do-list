18c496c810fde71e48cf0a7022e05389cb731dad
thayllachristine <thayllachristineo@gmail.com>
2018-09-26 11:49:21 -0300

    update script : checked tasks

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ addButton.addEventListener("click", function(event){
 
 //validação do input
 
-if (inputText.value === undefined || inputText.value === null || inputText.value === `` || inputText === ` `) {
+if (inputText.value === undefined || inputText.value === null || inputText.value === `` || inputText.value === ` `) {
     alert ("Escreva uma tarefa")
     inputText.focus();
     return false  
@@ -23,7 +23,7 @@ if (inputText.value === undefined || inputText.value === null || inputText.value
     
     const newTask = document.createElement("div")
     newTask.innerHTML = `<div class="tasklist__div">
-    <p> ${inputText.value} </p>
+    <p class="tasklist__p-none"> ${inputText.value} </p>
     <button class="btn btn__delete">x</button></div>`
 
     div.appendChild(newTask)
@@ -39,10 +39,36 @@ if (inputText.value === undefined || inputText.value === null || inputText.value
     deleteButton.addEventListener("click", function(event){
         event.preventDefault();
         div.remove();
-    
     })
 
+
+    const paragTask = document.querySelector(`.tasklist__p-none`) 
+    paragTask.onclick = function click_p(event){
+        event.preventDefault()
+
+        if (paragTask.classList.contains(`tasklist__p-none`)) {
+            paragTask.classList.remove(`tasklist__p-none`)
+            paragTask.classList.add(`tasklist__p-checked`)
+
+            paragTask.style.fontStyle = `italic`
+            paragTask.style.textDecoration = `line-through`
+            paragTask.style.color = `lightgray`
+            
+        } else {
+            paragTask.classList.remove(`tasklist__p-checked`)
+            paragTask.classList.add(`tasklist__p-none`)
+
+            paragTask.style.fontStyle = `normal`
+            paragTask.style.textDecoration = `none`
+            paragTask.style.color = `black`
+        }
+    }
+  
+    
+
 })
+
+
 
 
 
